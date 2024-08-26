0aace32abc5e14460f543e1c0393ac86973d8028
jungang0414 <dayr88520@gmail.com>
2024-08-26 18:29:41 +0800

    update: 新增全域Material CSS reset，在HomePage顯示資料庫中的域設資料

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import { CssBaseline } from "@mui/material";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
+    <CssBaseline />
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,46 @@
+import { db } from "../firebase/config";
+import { query, collection, onSnapshot, orderBy } from "firebase/firestore";
+import {
+  Box,
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+  Typography,
+} from "@mui/material";
+import { useEffect, useState } from "react";
+
 function HomePage() {
-  return <div>HomePage</div>;
+  const [newsList, setNewsList] = useState([]);
+
+  useEffect(() => {
+    const q = query(collection(db, "taichungNews"));
+    onSnapshot(q, (querySnapshots) => {
+      const data = [];
+      querySnapshots.forEach((doc) => {
+        data.push(doc.data());
+      });
+      setNewsList(data);
+    });
+  });
+
+  return (
+    <>
+      <Box>
+        {newsList.map((news) => (
+          <Accordion key={news.id}>
+            <AccordionSummary aria-controls="panel1-content" id="panel1-header">
+              <Typography>{news.name}</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography>
+                {news.content}
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
+        ))}
+      </Box>
+    </>
+  );
 }
 
 export default HomePage;
