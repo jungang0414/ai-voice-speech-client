f21c138af1d862c251cc08a21178090653f41e30
jungang0414 <dayr88520@gmail.com>
2024-08-30 12:33:21 +0800

    feat: 優化使用者體驗 修改後端API,改為正式環境使用API； 修改Firebase database的參數,依modifyDate排序,並限制前5筆資料； 設定獲取數據時的載入狀態,提升使用者體驗

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,11 @@
 import { db, auth } from "../firebase/config";
-import { query, collection, onSnapshot } from "firebase/firestore";
+import {
+  query,
+  collection,
+  onSnapshot,
+  orderBy,
+  limit,
+} from "firebase/firestore";
 import {
   Box,
   Accordion,
@@ -17,11 +23,20 @@ function HomePage() {
   //儲存使用者狀態變數
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [searchLoading, setSearchLoading] = useState(false);
 
   const [newsList, setNewsList] = useState([]);
+  // 本地使用
+  // const searchNewsApi = "http://localhost:3000/api/v1/AllNews"
+  // 正式環境替換為 Railway
+  const searchNewsApi = "https://ai-voice-speech-server-production.up.railway.app/api/v1/AllNews";
 
   useEffect(() => {
-    const q = query(collection(db, "taichungNews"));
+    const q = query(
+      collection(db, "taichungNews"),
+      orderBy("modifyDate", "desc"),
+      limit(5)
+    );
     const unsubscribe = onSnapshot(q, (querySnapshots) => {
       const data = [];
       querySnapshots.forEach((doc) => {
@@ -43,6 +58,17 @@ function HomePage() {
 
   const onSignOut = async () => {
     await signOut(auth);
+  };
+
+  const handleSearchAllNews = async () => {
+    setSearchLoading(true);
+    try {
+      await fetch(searchNewsApi);
+    } catch (error) {
+      console.error("Failed fetch news: ", error);
+    } finally {
+      setSearchLoading(false);
+    }
   };
 
   return (
@@ -73,19 +99,45 @@ function HomePage() {
             <SignIn />
           ) : (
             <Box>
-              {newsList.map((news) => (
-                <Accordion key={news.id}>
-                  <AccordionSummary
-                    aria-controls="panel1-content"
-                    id="panel1-header"
-                  >
-                    <Typography>{news.name}</Typography>
-                  </AccordionSummary>
-                  <AccordionDetails>
-                    <Typography>{news.content}</Typography>
-                  </AccordionDetails>
-                </Accordion>
-              ))}
+              <Box
+                sx={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  mb: 2,
+                }}
+              >
+                <Button
+                  variant="contained"
+                  onClick={handleSearchAllNews}
+                  sx={{ mt: 2 }}
+                >
+                  抓取當天新聞資料&回傳顯示
+                </Button>
+              </Box>
+              <Box>
+                {searchLoading ? (
+                  <Box>
+                    {searchLoading && <Typography>資料獲取中..</Typography>}
+                  </Box>
+                ) : (
+                  <Box>
+                    {newsList.map((news) => (
+                      <Accordion key={news.id}>
+                        <AccordionSummary
+                          aria-controls="panel1-content"
+                          id="panel1-header"
+                        >
+                          <Typography>{news.name}</Typography>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                          <Typography>{news.content}</Typography>
+                        </AccordionDetails>
+                      </Accordion>
+                    ))}
+                  </Box>
+                )}
+              </Box>
             </Box>
           )}
         </>
